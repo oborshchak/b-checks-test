65d36cab8400740c535523c2018ee6c4af51913b
Oleksandr Borshchak <oborshchak@amcbridge.com>
2024-08-22 15:04:19 +0300

    add React app

---
+++ jest.config.js
@@ -0,0 +1,11 @@
+export default {
+  collectCoverage: true,
+  collectCoverageFrom: ["./src/**"],
+  moduleNameMapper: {
+    "^@/(.*)$": "<rootDir>/src/$1",
+  },
+  testEnvironment: "jest-environment-jsdom",
+  transform: {
+    "^.+\\.[t|j]sx?$": "babel-jest",
+  },
+};
